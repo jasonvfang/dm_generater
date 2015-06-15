49c71bca3677583ffad002df1bc81ecf1a4b8250
jasonvfang <jasonvfang@gmail.com>
2015-06-15 23:36:08 +0800

    del useless fils

--- CWMP/dm_generator/DmObject.py
+++
@@ -1,7 +0,0 @@
-#coding=utf-8
-#global definition for data model object elements
-PARAMETER_ATTRS = ['type', 'access', 'notification', 'needReboot', 'accessList', 'style', 'defaultValue']
-XML_OBJ_TYPES = ['Object', 'MultiObject', 'Parameter', 'Instance']
-VALUE_TYPES = ['Unsigned', 'dataTime', 'String', 'Int', 'Bool']
-
-
